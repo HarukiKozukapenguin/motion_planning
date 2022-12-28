cf5a1a0a0f79015fab12e1275e467b04d42d37bd
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2022-12-28 13:22:41 +0900

    examine with multiple obstacles

--- aerial_motion/differential_kinematics/scripts/simple_collision.py
+++ aerial_motion/differential_kinematics/scripts/simple_collision.py
@@ -5,35 +5,42 @@ import time
 import rospy
 import math
 from visualization_msgs.msg import MarkerArray, Marker
+import pandas as pd
 
 if __name__ == "__main__":
 
     rospy.init_node("simple_collision")
+    file = rospy.get_param('~obstacle_world_file')
+    if file is None:
+        rospy.logerr("no valid obsracle world file")
+    df = pd.read_csv(file, header=None)
 
     obj_type = rospy.get_param("~type", 3) # 3: Sphere
-    position_x = rospy.get_param("~position_x", 0)
-    position_y = rospy.get_param("~position_y", 0)
-    scale_x = rospy.get_param("~scale_x", 0.1)
-    scale_y = rospy.get_param("~scale_y", 0.1)
-    scale_z = rospy.get_param("~scale_z", 0.1)
+    # position_x = rospy.get_param("~position_x", 0)
+    # position_y = rospy.get_param("~position_y", 0)
+    # scale_x = rospy.get_param("~scale_x", 0.1)
+    # scale_y = rospy.get_param("~scale_y", 0.1)
+    # scale_z = rospy.get_param("~scale_z", 0.1)
 
     pub = rospy.Publisher("/env_collision", MarkerArray, queue_size=10)
 
     time.sleep(1)
     env_obj = MarkerArray()
-    env_obj.markers.append(Marker())
-    env_obj.markers[0].header.frame_id = "/world"
-    env_obj.markers[0].type = obj_type
-    env_obj.markers[0].action = 0
-    env_obj.markers[0].pose.position.x = position_x
-    env_obj.markers[0].pose.position.y = position_y
-    env_obj.markers[0].pose.position.z = 0
-    env_obj.markers[0].pose.orientation.w = 1
-    env_obj.markers[0].scale.x = scale_x
-    env_obj.markers[0].scale.y = scale_y
-    env_obj.markers[0].scale.z = scale_z
-    env_obj.markers[0].color.r = 1
-    env_obj.markers[0].color.a = 1
+    for i in range(len(df)):
+        env_obj.markers.append(Marker())
+        env_obj.markers[i].id = i
+        env_obj.markers[i].header.frame_id = "world"
+        env_obj.markers[i].type = obj_type
+        env_obj.markers[i].action = 0
+        env_obj.markers[i].pose.position.x = df.loc[i, 1]
+        env_obj.markers[i].pose.position.y = df.loc[i, 2]
+        env_obj.markers[i].pose.position.z = 0
+        env_obj.markers[i].pose.orientation.w = 1
+        env_obj.markers[i].scale.x = df.loc[i, 8]
+        env_obj.markers[i].scale.y = df.loc[i, 8]
+        env_obj.markers[i].scale.z = 0.3
+        env_obj.markers[i].color.r = 1
+        env_obj.markers[i].color.a = 1
 
     while not rospy.is_shutdown():
 
